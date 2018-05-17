d0e88fdd4ed5dd207abf99fa4a34cbcdbbe6bf74
Gabin An <agb94@kaist.ac.kr>
2018-05-17 23:59:08 +0900

    Add type error parser

--- covgen/profiler.py
+++ covgen/profiler.py
@@ -2,11 +2,14 @@ import itertools
 import ast
 import astor
 
+
 class Profiler(ast.NodeTransformer):
     def __init__(self):
         super()
         self.branches = dict()
         self.branch_id = 1
+        self.current_lineno = None
+        self.line_and_vars = dict()
 
     def visit_predicate(self, expr_node, depth=0):
         bid_multiplier = -1 if depth > 0 else 1
@@ -15,30 +18,61 @@ class Profiler(ast.NodeTransformer):
                 left_node = ast.Compare(
                     left=expr_node.left,
                     ops=expr_node.ops[:-1],
-                    comparators=expr_node.comparators[:-1]
-                )
+                    comparators=expr_node.comparators[:-1])
             else:
                 left_node = expr_node.left
             expr_node = ast.Call(
-                func = ast.Attribute(value = ast.Name(id='covw', ctx=ast.Load()), attr='comparison', ctx=ast.Load()),
-                args=[ast.Num(n = bid_multiplier * self.branch_id), ast.Num(n = depth), ast.Str(s = expr_node.ops[-1].__class__.__name__), left_node, expr_node.comparators[-1]],
+                func=ast.Attribute(
+                    value=ast.Name(id='covw', ctx=ast.Load()),
+                    attr='comparison',
+                    ctx=ast.Load()),
+                args=[
+                    ast.Num(n=bid_multiplier * self.branch_id),
+                    ast.Num(n=depth),
+                    ast.Str(s=expr_node.ops[-1].__class__.__name__), left_node,
+                    expr_node.comparators[-1]
+                ],
                 keywords=[])
         elif isinstance(expr_node, ast.BoolOp):
             for i, value in enumerate(expr_node.values):
                 expr_node.values[i] = self.visit_predicate(value, depth + 1)
             expr_node = ast.Call(
-                func = ast.Attribute(value = ast.Name(id='covw', ctx=ast.Load()), attr='boolop', ctx=ast.Load()),
-                args=[ast.Num(n = bid_multiplier * self.branch_id), ast.Num(n = depth), ast.Str(s = expr_node.op.__class__.__name__), ast.List(elts=expr_node.values, ctx=ast.Load())],
+                func=ast.Attribute(
+                    value=ast.Name(id='covw', ctx=ast.Load()),
+                    attr='boolop',
+                    ctx=ast.Load()),
+                args=[
+                    ast.Num(n=bid_multiplier * self.branch_id),
+                    ast.Num(n=depth),
+                    ast.Str(s=expr_node.op.__class__.__name__),
+                    ast.List(elts=expr_node.values, ctx=ast.Load())
+                ],
                 keywords=[])
-        elif isinstance(expr_node, ast.UnaryOp) and isinstance(expr_node.op, ast.Not):
+        elif isinstance(expr_node, ast.UnaryOp) and isinstance(
+                expr_node.op, ast.Not):
             expr_node = ast.Call(
-                func = ast.Attribute(value = ast.Name(id='covw', ctx=ast.Load()), attr='unaryop', ctx=ast.Load()),
-                args=[ast.Num(n = bid_multiplier * self.branch_id), ast.Num(n = depth), ast.Str(s = expr_node.op.__class__.__name__), self.visit_predicate(expr_node.operand, depth + 1)],
+                func=ast.Attribute(
+                    value=ast.Name(id='covw', ctx=ast.Load()),
+                    attr='unaryop',
+                    ctx=ast.Load()),
+                args=[
+                    ast.Num(n=bid_multiplier * self.branch_id),
+                    ast.Num(n=depth),
+                    ast.Str(s=expr_node.op.__class__.__name__),
+                    self.visit_predicate(expr_node.operand, depth + 1)
+                ],
                 keywords=[])
-        elif isinstance(expr_node, ast.Name) or isinstance(expr_node, ast.Call):
+        elif isinstance(expr_node, ast.Name) or isinstance(
+                expr_node, ast.Call):
             expr_node = ast.Call(
-                func = ast.Attribute(value = ast.Name(id='covw', ctx=ast.Load()), attr='value', ctx=ast.Load()),
-                args=[ast.Num(n = bid_multiplier * self.branch_id), ast.Num(n = depth), expr_node],
+                func=ast.Attribute(
+                    value=ast.Name(id='covw', ctx=ast.Load()),
+                    attr='value',
+                    ctx=ast.Load()),
+                args=[
+                    ast.Num(n=bid_multiplier * self.branch_id),
+                    ast.Num(n=depth), expr_node
+                ],
                 keywords=[])
         else:
             raise Exception("Unsupported Branch Predicate")
@@ -55,23 +89,54 @@ class Profiler(ast.NodeTransformer):
 
     def visit_If(self, node):
         return self.visit_branch_node(node)
-    
+
     def visit_While(self, node):
         return self.visit_branch_node(node)
-    
+
     def visit_For(self, node):
         iter_node = ast.Call(
-            func = ast.Attribute(value = ast.Name(id='covw', ctx=ast.Load()), attr='iter', ctx=ast.Load()),
-            args=[ast.Num(n = self.branch_id), node.iter],
+            func=ast.Attribute(
+                value=ast.Name(id='covw', ctx=ast.Load()),
+                attr='iter',
+                ctx=ast.Load()),
+            args=[ast.Num(n=self.branch_id), node.iter],
             keywords=[])
         self.branches[node] = self.branch_id
         self.branch_id += 1
         node.iter = iter_node
         self.generic_visit(node)
         return ast.fix_missing_locations(node)
-    
+
+    def collect_var_names(self, node):
+        var_names = set()
+
+        def visit_all_attr(node):
+            if hasattr(node, 'lineno'):
+                self.current_lineno = node.lineno
+
+            if not hasattr(node, '__dict__'):
+                return
+            if isinstance(node, ast.Name):
+                self.line_and_vars[
+                    self.current_lineno] = self.line_and_vars.get(
+                        self.current_lineno, set())
+                self.line_and_vars[self.current_lineno].add(node.id)
+                return
+            node_vars = vars(node)
+            for k in node_vars:
+                if isinstance(node_vars[k], list):
+                    for stmt in node_vars[k]:
+                        visit_all_attr(stmt)
+                else:
+                    visit_all_attr(node_vars[k])
+
+        visit_all_attr(node)
+        return self.line_and_vars
+
+    """
     def collect_int_constants(self, node):
         self.int_constants = set()
+
         def visit_all_attr(node):
             if not hasattr(node, '__dict__'):
                 return
@@ -86,8 +151,10 @@ class Profiler(ast.NodeTransformer):
                         visit_all_attr(stmt)
                 else:
                     visit_all_attr(node_vars[k])
+
         visit_all_attr(node)
         return self.int_constants
+    """
 
     def instrument(self, sourcefile, inst_sourcefile, function):
         def get_source(path):
@@ -98,7 +165,8 @@ class Profiler(ast.NodeTransformer):
         root = ast.parse(source)
 
         # Insert 'import covgen.wrapper as covw' in front of the file
-        import_node = ast.Import(names=[ast.alias(name='covgen.wrapper', asname='covw')]) 
+        import_node = ast.Import(
+            names=[ast.alias(name='covgen.wrapper', asname='covw')])
         root.body.insert(0, import_node)
         ast.fix_missing_locations(root)
 
@@ -108,12 +176,27 @@ class Profiler(ast.NodeTransformer):
                 function_node = stmt
                 break
         assert function_node
-        
+        """
         self.collect_int_constants(function_node)
+        """
         self.visit(function_node)
-        total_branches = { k: None for k in list(itertools.product(range(1, self.branch_id), [True, False])) }
+        total_branches = {
+            k: None
+            for k in list(
+                itertools.product(range(1, self.branch_id), [True, False]))
+        }
 
         with open(inst_sourcefile, 'w') as instrumented:
             instrumented.write(astor.to_source(root))
-        
+
+        inst_source = get_source(inst_sourcefile)
+        root = ast.parse(inst_source)
+        inst_function_node = None
+        for stmt in root.body:
+            if isinstance(stmt, ast.FunctionDef) and stmt.name == function:
+                inst_function_node = stmt
+                break
+        assert inst_function_node
+        self.collect_var_names(inst_function_node)
+
         return function_node, total_branches
--- main.py
+++ main.py
@@ -7,20 +7,25 @@ import os
 import sys
 import traceback
 import re
+import inspect
+
 
 def run(function, input_value, total_branches, timeout=5):
     import signal
 
     class Branch:
-        def __init__(self, branch_id: int, depth: int, predicate_result: bool, op: str, branch_distance: int):
+        def __init__(self, branch_id: int, depth: int, predicate_result: bool,
+                     op: str, branch_distance: int):
             self.id = branch_id
             self.depth = depth
             self.predicate_result = predicate_result
             self.op = op
             self.branch_distance = branch_distance
-        
+
         def __str__(self):
-            return "{}\t{}\t{}\t{}\t{}".format(self.id, self.depth, self.predicate_result, self.op, self.branch_distance)
+            return "{}\t{}\t{}\t{}\t{}".format(self.id, self.depth,
+                                               self.predicate_result, self.op,
+                                               self.branch_distance)
 
         def to_tuple(self):
             return (self.id, self.predicate_result)
@@ -28,8 +33,13 @@ def run(function, input_value, total_branches, timeout=5):
         @classmethod
         def parse_line(cls, l: str):
             cols = l.strip().split('\t')
-            return cls(int(cols[0]), int(cols[1]), bool(int(cols[2])), cols[3], { True: float(cols[4]), False: float(cols[5]) })
-   
+            return cls(
+                int(cols[0]),
+                int(cols[1]),
+                bool(int(cols[2])), cols[3],
+                {True: float(cols[4]),
+                 False: float(cols[5])})
+
     def clear_coverage_report():
         open('.cov', 'w').close()
 
@@ -45,7 +55,7 @@ def run(function, input_value, total_branches, timeout=5):
         raise Exception("end of time")
 
     clear_coverage_report()
-    
+
     signal.signal(signal.SIGALRM, handler)
     signal.alarm(timeout)
     try:
@@ -55,18 +65,26 @@ def run(function, input_value, total_branches, timeout=5):
             return (False, None)
         else:
             exc_type, exc_value, exc_traceback = sys.exc_info()
-            for elem in traceback.format_exception(exc_type, exc_value, exc_traceback):
+            for elem in traceback.format_exception(exc_type, exc_value,
+                                                   exc_traceback):
                 if function.__name__ in elem:
                     lineno = int(elem.split(',')[1].strip().split()[-1])
-            op, type1, type2 = tuple(map(lambda x: x.strip("'"), filter(lambda x: x.startswith("'") and x.endswith("'"), str(exc_value).split())))
+                    break
+            op, type1, type2 = tuple(
+                map(lambda x: x.strip("'"),
+                    filter(lambda x: x.startswith("'") and x.endswith("'"),
+                           str(exc_value).split())))
             return (False, (lineno, op, type1, type2))
     cov_result = read_coverage_report()
     for b in cov_result:
         total_branches[b.to_tuple()] = tuple(input_value)
     return (True, cov_result)
 
+
 def next_target(branches, cannot_cover):
-    not_covered = list(filter(lambda b: not b[1] and not b[0] in cannot_cover, branches.items()))
+    not_covered = list(
+        filter(lambda b: not b[1] and not b[0] in cannot_cover,
+               branches.items()))
     if not_covered:
         return not_covered[0][0]
     else:
@@ -75,32 +93,38 @@ def next_target(branches, cannot_cover):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Coverage Measurement Tool')
-    parser.add_argument('sourcefile', type=str, help='a file path to instrument')
+    parser.add_argument(
+        'sourcefile', type=str, help='a file path to instrument')
     parser.add_argument('function', type=str, help='target function name')
     args = parser.parse_args()
-    
+
     profiler = Profiler()
     inst_sourcefile = os.path.join(
         os.path.dirname(args.sourcefile),
         'inst_' + os.path.basename(args.sourcefile))
-    function_node, total_branches = profiler.instrument(args.sourcefile, inst_sourcefile, args.function)
+    function_node, total_branches = profiler.instrument(
+        args.sourcefile, inst_sourcefile, args.function)
     cfg = get_cfg(function_node, profiler.branches)
-    target_module = importlib.import_module(inst_sourcefile.rstrip('.py').replace('/', '.'))
-    
-    
-    cannot_cover=set()
+    target_module = importlib.import_module(
+        inst_sourcefile.rstrip('.py').replace('/', '.'))
+
+    # Set target branch
     target_branch = (1, False)
-    success, result = run(target_module.__dict__[args.function], ['abc'], total_branches)
-    print(result)
+    # Set input
+    function_input = ['abc']
+    # Run the instrumented function
+    success, result = run(target_module.__dict__[args.function], function_input,
+                          total_branches)
     if success:
+        # No Error
         cov_result = result
     elif result:
+        # Type Error
         lineno, op, type1, type2 = result
-        
-    """
-    for i in range(0, 9):
-        print (i)
-        cov_report = run(target_module.__dict__[args.function], [i], total_branches)
-        fitness = get_fitness(cfg, target_branch, cov_report)
-        print (fitness)
-        """
+        suspicous_inputs = set()
+        args = inspect.getargspec(target_module.__dict__[args.function]).args
+        for v in profiler.line_and_vars[lineno]:
+            if v in args:
+                suspicous_inputs.add(args.index(v))
+        print(lineno, op, type1, type2)
+        print(suspicous_inputs)
