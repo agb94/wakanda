5579ff54a92a359d161c27d354e20049d9ad2d9b
Jisu <jsok1994@kaist.ac.kr>
2018-06-01 16:46:27 +0900

    fixed missing 'if' in dist fuction

--- covgen/wrapper.py
+++ covgen/wrapper.py
@@ -60,7 +60,8 @@ def iter(bid, depth, expr):
 
 ## Suppose a, b are given with 'valid' type -> one of int, float, str, list, tuple
 def dist(a, b = None):
-    b = _type(type(a)).get()
+    if not b:
+        b = _type(type(a)).get()
 
     # numerical values
     if (type(a) is int or type(a) is float or type(a) is bool) and (type(b) is int or type(b) is float or type(b) is bool) :
