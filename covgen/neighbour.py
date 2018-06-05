ad5842d47292b7dbdf3159a9afbe00e7e2efe743
Jisu <jsok1994@kaist.ac.kr>
2018-06-05 16:08:07 +0900

    fixed tab size

--- covgen/neighbour.py
+++ covgen/neighbour.py
@@ -3,56 +3,56 @@ import itertools
 # returns 'neighbour' of given input value with ragne 'n' (+/- n)
 # ex) get_Neighbours([3, 'e'], 1) = [[2, 'd'], [2, 'e'], [2, 'f'], [3, 'd'], [3, 'e'], [3, 'f'], [4, 'd'], [4, 'e'], [4, 'f']]
 def get_Neighbours(vals, n):
-	assert type(n) is int
-	assert n > 0
+    assert type(n) is int
+    assert n > 0
 
-	if len(vals) == 1:
-		v = vals[0]
+    if len(vals) == 1:
+        v = vals[0]
 		
-		if type(v) is int:
-			res = []
-			for i in range(-n, n+1):
-				res.append([v + i])
-			return res
+        if type(v) is int:
+            res = []
+            for i in range(-n, n+1):
+                res.append([v + i])
+            return res
 
-		# needs to be changed
-		elif type(v) is float:
-			res = []
-			for i in range(-n, n+1):
-				res.append([v + i*0.00000000000001])
-			return res
+        # needs to be changed
+        elif type(v) is float:
+            res = []
+            for i in range(-n, n+1):
+                res.append([v + i*0.00000000000001])
+            return res
 
-		elif type(v) is str and len(v) == 1:
-			res = []
-			for i in range(-n, n+1):
-				res.append([chr(ord(v) + i)])
-			return res
+        elif type(v) is str and len(v) == 1:
+            res = []
+            for i in range(-n, n+1):
+                res.append([chr(ord(v) + i)])
+            return res
 
-		elif type(v) is bool:
-			return [True, False]
+        elif type(v) is bool:
+            return [True, False]
 
-		elif type(v) is str or type(v) is list or type(v) is tuple:
-			nei = get_Neighbours(v, n)
-			lst = []
-			for e in nei:
-				lst.append([e])
+        elif type(v) is str or type(v) is list or type(v) is tuple:
+            nei = get_Neighbours(v, n)
+            lst = []
+            for e in nei:
+                lst.append([e])
 
-			return lst
+            return lst
 
 
 
-	else:
-		lst = []
-		for v in vals:
-			neighbour = get_Neighbours([v], n)
-			l = []
-			for e in neighbour:
-				if (len(e) != 1):
-					raise Exception("Something wrong in get_Neighbours")
-				l.append(e[0])
-			lst.append(l)
-		pd = itertools.product(*lst)
-		res = []
-		for e in pd:
-			res.append(list(e))
-		return res
+    else:
+        lst = []
+        for v in vals:
+            neighbour = get_Neighbours([v], n)
+            l = []
+            for e in neighbour:
+                if (len(e) != 1):
+                    raise Exception("Something wrong in get_Neighbours")
+                l.append(e[0])
+            lst.append(l)
+        pd = itertools.product(*lst)
+        res = []
+        for e in pd:
+            res.append(list(e))
+        return res
