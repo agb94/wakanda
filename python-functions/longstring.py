19b7826ee1485fcaea411177222282bcaadfeded
Jisu <jsok1994@kaist.ac.kr>
2018-06-10 19:55:19 +0900

    add some new test functions for string input

---
+++ python-functions/longstring.py
@@ -0,0 +1,6 @@
+def longstring(s):
+	a = "-"
+	b = "ab"
+	c = a*10 + b*10
+	if s == c:
+		return True
---
+++ python-functions/string1.py
@@ -0,0 +1,7 @@
+def string1(s):
+	if s == "string test":
+		return True
+	elif s == "string test 2":
+		return False
+	elif s >= "string!!!!!":
+		return 1
